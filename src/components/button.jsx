7b9e6182d59ff59b8713f7cfd100d8ece8757303
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2023-01-03 13:16:11 -0300

    hooks funcionales

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Button = () => {
+    
     const handleClick = () => {
         console.log("me diste click")
 
@@ -8,7 +9,7 @@ const Button = () => {
     return (
         <div className="p-3" >
             <div action="form-group">
-            <button id="btn" type="submit" className="btn btn-primary " onClick={(handleClick)}>Submit</button>
+            <button id="btn" type="submit" className="btn btn-primary disabled" onClick={(handleClick)}>Submit</button>
             </div>
         </div>
         )
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 
 const Input = () => {
-    const [contraseña, setContraseña] = useState("")
     
+    const [nombre, setNombre] = useState("");
+    const [contraseña, setContraseña] = useState("");
+
+    const validarContraseña = (e) => {
+        
+        if(contraseña === '252525') {
+            alert('contraseña correcta')
+        } else {
+            alert('Error: contraseña incorrecta')
+        }
+    }    
+
     return (
         <div>
-            <form>
+            <div>
                 <div className="form-group p-3">
                     <label htmlFor="exampleInputEmail1">Nombre</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa tu nombre"></input>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa tu nombre" onChange={(e) => setNombre(e.target.value)}></input>
                 </div>
                 <div className="form-group p-3">
                     <label htmlFor="exampleInputPassword1">Contraseña</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Ingresa tu contraseña" onChange={(e) => setContraseña(e.target.value)}></input>
                 </div>
-            </form>
+                <div action="form-group">
+                    <button id="btn" type="submit" className="btn btn-primary " onClick={() => {validarContraseña()}}>ver datos</button>
+                </div>
+            </div>
         </div>
     )
 }
